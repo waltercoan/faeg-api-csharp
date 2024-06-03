23753c73ea72fbe1c67a4662eb554c6baff3b9df
Walter Silvestre Coan <walter.coan@gmail.com>
2024-06-03 16:51:10 -0300

    first commit

---
+++ apiapp/Program.cs
@@ -0,0 +1,57 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  =>
+                      {
+                          policy.WithOrigins("*");
+                      });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger(); 
+app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+https://gist.github.com/taoyuan/39d9bc24bafc8cc45663683eae36eb1a
+
+
+
+
+```
+    "HttpsInlineCertAndKeyFile": {
+        "Url": "https://+:5002",
+        "Certificate": {
+          "Path": "cert/cert.crt",
+          "KeyPath": "cert/cert.key",
+          "AllowInvalid": true
+        }
+      }
+
+```
